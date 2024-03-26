ee15a161c9dbcdbff74eb8402d5e7c99b8dd95ee
1ouiss <ls.bouet@gmail.com>
2024-03-26 22:51:52 +0100

    fix: handleChange input signin component

--- src/components/Auth/Signin/Signin.tsx
+++ src/components/Auth/Signin/Signin.tsx
@@ -1,4 +1,9 @@
-import { UserCredential, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  UserCredential,
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import PageLayout from "../../PageLayout/PageLayout";
 import { Button, Text, TextInput } from "react-native-paper";
 import * as React from "react";
@@ -6,70 +11,75 @@ import { StyleSheet } from "react-native";
 import { NavigationProps } from "../../../../types/types";
 import { useState, FC } from "react";
 import { auth } from "../../../firebase";
+import { NavigationProp } from "@react-navigation/native";
 
+const Signin: FC<{
+  setIsSignup: React.Dispatch<React.SetStateAction<boolean>>;
+  navigation: NavigationProp<any>;
+}> = ({ setIsSignup, navigation }) => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
+  const handleChange = (text: string, key: string) => {
+    setUser({
+      ...user,
+      [key]: text,
+    });
+  };
 
-const Signin: FC<{ setIsSignup: React.Dispatch<React.SetStateAction<boolean>>, navigation: any }> = ({ setIsSignup, navigation }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState("");
-
-    const handleChangeEmail = (text: string) => {
-        setEmail(text);
-    };
-
-    const handleChangePassword = (text: string) => {
-        setPassword(text);
-    };
-
-    const handleSubmit = async () => {
-        let response = {};
-        let userConected = null;
-        try {
-            await signInWithEmailAndPassword(auth, email, password)
-            .then(async (userCredential: UserCredential) => {
-              userConected = userCredential.user;
-              console.log(userConected.uid);
-            }) 
-            
-        } catch (err) {
-            setErrorMessage("Une erreur inattendue s'est produite.");
+  const handleSubmit = async () => {
+    let response = {};
+    let userConected = null;
+    try {
+      await signInWithEmailAndPassword(auth, user.email, user.password).then(
+        async (userCredential: UserCredential) => {
+          userConected = userCredential.user;
+          console.log(userConected.uid);
         }
-    };
-  
-      
-
-    return ( 
-        <>
-            <TextInput
-                style={styles.input}
-                label="Email"
-                mode='outlined'
-                value={email}
-                onChangeText={handleChangeEmail}
-            />
-            <TextInput
-                style={styles.input}
-                label="Password"
-                mode='outlined'
-                value={password}
-                onChangeText={handleChangePassword}
-            />
-            <Button mode="contained" onPress={handleSubmit}>
-                Se connecter
-            </Button>
-            {error && <Text>{errorMessage}</Text>}
-            <Text variant="bodyMedium">Vous n'avez pas de compte ?<Text onPress={() => setIsSignup(false)}>Inscrivez-vous</Text></Text>
-        </>
-     );
+      );
+    } catch (err) {
+      setErrorMessage("Une erreur inattendue s'est produite.");
     }
-    const styles = StyleSheet.create({
-       input: {
-           width: 300,
-           height: 44,
-           padding: 10,
-           marginBottom: 10,
-       },
-   });
-export default Signin
+  };
+
+  return (
+    <>
+      <TextInput
+        style={styles.input}
+        label="Email"
+        mode="outlined"
+        value={user.email}
+        onChangeText={(e) => handleChange(e, "email")}
+      />
+      <TextInput
+        style={styles.input}
+        label="Password"
+        mode="outlined"
+        value={user.password}
+        onChangeText={(e) => handleChange(e, "password")}
+        secureTextEntry
+      />
+      <Button mode="contained" onPress={handleSubmit}>
+        Se connecter
+      </Button>
+      {error && <Text>{errorMessage}</Text>}
+      <Text variant="bodyMedium">
+        Vous n'avez pas de compte ?
+        <Text onPress={() => setIsSignup(false)}>Inscrivez-vous</Text>
+      </Text>
+    </>
+  );
+};
+const styles = StyleSheet.create({
+  input: {
+    width: 300,
+    height: 44,
+    padding: 10,
+    marginBottom: 10,
+  },
+});
+export default Signin;
--- src/components/Auth/Signup/Signup.tsx
+++ src/components/Auth/Signup/Signup.tsx
@@ -3,108 +3,107 @@ import PageLayout from "../../PageLayout/PageLayout";
 import { Button, Text, TextInput } from "react-native-paper";
 import * as React from "react";
 import { StyleSheet } from "react-native";
-import { NavigationProps } from "../../../../types/types";
 import { updateDoc } from "../../../database/set";
 import { auth } from "../../../firebase";
 import { useState, FC } from "react";
+import { NavigationProp } from "@react-navigation/native";
 
+const Signup: FC<{
+  setIsSignup: React.Dispatch<React.SetStateAction<boolean>>;
+  navigation: NavigationProp<any>;
+}> = ({ setIsSignup, navigation }) => {
+  const [user, setUser] = useState({
+    id: "",
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+  });
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-
-const Signup: FC<{ setIsSignup: React.Dispatch<React.SetStateAction<boolean>>, navigation: any }> = ({ setIsSignup, navigation }) => {
-
-    const [user, setUser] = useState({
-        id: "",
-        email: "",
-        password: "",
-        firstName: "",
-        lastName: "",
+  const handleChange = (text: string, key: string) => {
+    setUser({
+      ...user,
+      [key]: text,
     });
-    const [error, setError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState("");
-    
-    // const auth = getAuth();
+  };
 
-    const handleChange = (text: string, key: string) => {
-        setUser({
-            ...user,
-            [key]: text,
-        });
+  const handleSubmit = async () => {
+    try {
+      // Créer un nouvel utilisateur avec l'email et le mot de passe
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        user.email,
+        user.password
+      );
+      console.log(userCredential);
+      const newUser = userCredential.user;
+
+      await updateDoc({
+        newDatas: {
+          id: newUser.uid,
+          email: newUser.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+        },
+        collectionId: "users",
+        docId: newUser.uid,
+      });
+    } catch (err) {
+      console.log(err);
+
+      setErrorMessage("Une erreur inattendue s'est produite.");
     }
+  };
 
-    const handleSubmit = async () => {
-        try {
-            // Créer un nouvel utilisateur avec l'email et le mot de passe
-            const userCredential = await createUserWithEmailAndPassword(
-                auth,
-                user.email,
-                user.password
-            );
-            console.log(userCredential);
-            const newUser = userCredential.user;
-            
-
-            await updateDoc({
-                newDatas: {
-                  id: newUser.uid,
-                  email: newUser.email,
-                  firstName: user.firstName,
-                  lastName: user.lastName,
-                },
-                collectionId: "users",
-                docId: newUser.uid,
-              });
-            
-        } catch (err) {
-            console.log(err);
-            
-            setErrorMessage("Une erreur inattendue s'est produite.");
-        }
-    };
-
-    return ( 
-        <>
-            <TextInput
-                style={styles.input}
-                label="Email"
-                mode='outlined'
-                value={user.email}
-                onChangeText={(e)=>handleChange(e, "email")}
-            />
-            <TextInput
-                style={styles.input}
-                label="Password"
-                mode='outlined'
-                value={user.password}
-                onChangeText={(e)=>handleChange(e, "password")}
-            />
-            <TextInput
-                style={styles.input}
-                label="FirstName"
-                mode='outlined'
-                value={user.firstName}
-                onChangeText={(e)=>handleChange(e, "firstName")}
-            />
-            <TextInput
-                style={styles.input}
-                label="LastName"
-                mode='outlined'
-                value={user.lastName}
-                onChangeText={(e)=>handleChange(e, "lastName")}
-            />
-            <Button mode="contained" onPress={handleSubmit}>
-                S'inscrire
-            </Button>
-            {error && <Text>{errorMessage}</Text>}
-            <Text variant="bodyMedium">Déjà un compte ? <Text onPress={() => setIsSignup(true)}>Se connecter</Text></Text>
-        </>
-     );
-    }
-    const styles = StyleSheet.create({
-       input: {
-           width: 300,
-           height: 44,
-           padding: 10,
-           marginBottom: 10,
-       },
-   });
-export default Signup
+  return (
+    <>
+      <TextInput
+        style={styles.input}
+        label="Email"
+        mode="outlined"
+        value={user.email}
+        onChangeText={(e) => handleChange(e, "email")}
+      />
+      <TextInput
+        style={styles.input}
+        label="Password"
+        mode="outlined"
+        value={user.password}
+        onChangeText={(e) => handleChange(e, "password")}
+      />
+      <TextInput
+        style={styles.input}
+        label="FirstName"
+        mode="outlined"
+        value={user.firstName}
+        onChangeText={(e) => handleChange(e, "firstName")}
+      />
+      <TextInput
+        style={styles.input}
+        label="LastName"
+        mode="outlined"
+        value={user.lastName}
+        onChangeText={(e) => handleChange(e, "lastName")}
+      />
+      <Button mode="contained" onPress={handleSubmit}>
+        S'inscrire
+      </Button>
+      {error && <Text>{errorMessage}</Text>}
+      <Text variant="bodyMedium">
+        Déjà un compte ?{" "}
+        <Text onPress={() => setIsSignup(true)}>Se connecter</Text>
+      </Text>
+    </>
+  );
+};
+const styles = StyleSheet.create({
+  input: {
+    width: 300,
+    height: 44,
+    padding: 10,
+    marginBottom: 10,
+  },
+});
+export default Signup;
