0590d1336e3c4fbb2e2bf00db8b1bc180f6dd49a
1ouiss <ls.bouet@gmail.com>
2024-03-27 23:30:59 +0100

    feat: add content cocktail page

--- src/pages/Cocktail/Cocktail.tsx
+++ src/pages/Cocktail/Cocktail.tsx
@@ -1,7 +1,14 @@
 import { NavigationProp } from "@react-navigation/native";
 import { IconArrowBack, IconArrowLeft } from "@tabler/icons-react-native";
-import { Pressable, StyleSheet, View } from "react-native";
-import { Button, Text } from "react-native-paper";
+import {
+  ImageBackground,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  View,
+} from "react-native";
+import { Button, Chip, Text } from "react-native-paper";
+import { IngredientType } from "../../../types/types";
 
 const Cocktail = ({
   route,
@@ -14,13 +21,51 @@ const Cocktail = ({
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Pressable onPress={() => navigation.navigate("home")}>
+        <Pressable onPress={() => navigation.goBack()}>
           <IconArrowLeft size={24} />
         </Pressable>
         <Text variant="titleLarge">{cocktail.name}</Text>
       </View>
-      <Text>{cocktail.name}</Text>
-      <Button onPress={() => navigation.navigate("home")}>Go back</Button>
+      <ScrollView
+        style={{
+          width: "100%",
+          padding: 10,
+        }}
+      >
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: "100%",
+            height: 400,
+            borderRadius: 16,
+          }}
+        >
+          <ImageBackground
+            source={{ uri: cocktail.image }}
+            imageStyle={{ borderRadius: 16 }}
+            style={{
+              width: "100%",
+              height: "100%",
+              borderRadius: 24,
+            }}
+            resizeMode="cover"
+          />
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 10,
+          }}
+        >
+          {cocktail.ingredients.map((ingredient: IngredientType) => (
+            <Chip key={ingredient.id}>{ingredient.name}</Chip>
+          ))}
+        </View>
+        <Text>{cocktail.description}</Text>
+      </ScrollView>
     </View>
   );
 };
